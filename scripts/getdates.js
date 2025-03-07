ec37da123fe54f401858a77cb2a3f8e4eec6edc8
RBennyjay <ejohn1@byupathway.edu>
2025-03-07 03:42:44 +0100

    home-page

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+ // JavaScript to dynamically populate the year
+document.getElementById("current-year").textContent = new Date().getFullYear();
+
+// JavaScript to dynamically update the last modified date
+ document.getElementById("last-modified").textContent = "Last Modified: " + document.lastModified;
+    
